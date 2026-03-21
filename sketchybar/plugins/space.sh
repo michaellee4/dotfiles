2553d8dfb48ad0a0f511fd1290db9677d9eb6b80
Michael Lee <michaellee@mac.lan>
2026-03-20 23:30:25 -0400

    date

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+WS=$1
+FOCUSED=${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}
+AERO_MONITOR=$(aerospace list-workspaces --all --format '%{workspace} %{monitor-id}' | awk -v ws="$WS" '$1 == ws {print $2}')
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ -z "$AERO_MONITOR" ]; then
+  if [ "$WS" = "$FOCUSED" ]; then
+    # Newly created workspace - get the currently focused monitor
+    AERO_MONITOR=$(aerospace list-monitors --focused --format '%{monitor-id}' 2>/dev/null)
+  fi
+  if [ -z "$AERO_MONITOR" ]; then
+    sketchybar --set "$NAME" drawing=off
+    exit 0
+  fi
+fi
+
+# Map aerospace monitor ID to sketchybar display ID
+# Aerospace monitor names contain "(N)" where N is the sketchybar arrangement-id
+DISPLAY=$(aerospace list-monitors --format '%{monitor-id} %{monitor-name}' | awk -v mid="$AERO_MONITOR" '$1 == mid {print $0}' | sed 's/.*(\([0-9]*\))/\1/')
+DISPLAY=${DISPLAY:-$AERO_MONITOR}
+
+if [ "$WS" = "$FOCUSED" ]; then
+  sketchybar --set "$NAME" \
+    drawing=on \
+    display=$DISPLAY \
+    background.color=0xffcba6f7 \
+    icon.color=0xff1e1e2e
+else
+  sketchybar --set "$NAME" \
+    drawing=on \
+    display=$DISPLAY \
+    background.color=0x40ffffff \
+    icon.color=0xffffffff
+fi
